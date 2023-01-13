86e2f27645534f6ebdf5b9e01f08f0652221518b
Jessica Bauman <jbauman815@gmail.com>
2023-01-12 19:49:39 -0500

    adding update with images

--- Starter_Code/Starter_Code/web/static/js/app.js
+++ Starter_Code/Starter_Code/web/static/js/app.js
@@ -30,16 +30,22 @@ var filter = {};
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-    let changeElement = d3.select(this)
+    let changeElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
-    let valueElement = 
+    let valueElement = changeElement.property("value");
+    console.log(valueElement);
     // 4c. Save the id of the filter that was changed as a variable.
-
+    let idFilter = changeElement.attr("id");
+    console.log(idFilter);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
- 
-  
+    if (valueElement) {
+      filter[idFilter] = valueElement;
+    }
+    else {
+      delete filter[idFilter];
+    }
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -49,18 +55,20 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filterData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    Object.entries(filter).forEach(([key, value]) => {
+      filterData = filterData.filter(row => row[key] === value); 
+    })
   
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filterData);
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.selectAll("input").on("change", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
